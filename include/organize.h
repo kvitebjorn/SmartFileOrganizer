4a65dfe28518d7f96e7bac633929e6807d3773b3
Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-02-24 16:23:11 -0300

    Add organizer header file

---
+++ include/organize.h
@@ -0,0 +1,13 @@
+#ifndef SFO_ORGANIZE_H
+#define SFO_ORGANIZE_H
+#include <string.h>
+#include <stdint.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "file_type_detector.h"
+#include "errors.h"
+
+int8_t organize(const char *path);
+
+#endif
