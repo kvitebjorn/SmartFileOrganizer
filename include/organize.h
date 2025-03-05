761aece8e119e5d65935af360ead41735092c927
henrique-electric <146463048+henrique-electric@users.noreply.github.com>
2025-03-05 00:36:16 +0000

    Created some errors and a array to hold the mime types for images

--- include/organize.h
+++ include/organize.h
@@ -10,6 +10,14 @@
 #include "file_type_detector.h"
 #include "errors.h"
 
+#define FAIL_HANDLE_TYPE -1
+#define FAIL_ORGANIZE_FILES -1
+
+#define IMAGE_TYPE_ARRAY_LEN 7
+
+const char *image_types[] =  {"png", "jpeg", "gif", "bmp", "tiff", "webp", "x-icon"};
+
+
 int8_t organize(const char *path);
 
 #endif
